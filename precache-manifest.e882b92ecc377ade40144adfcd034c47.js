0f228fe1a2feabea69c74f9931b650e93da17f8f
Serhii <davydko.serhiy@gmail.com>
2019-07-17 17:10:59 +0300

    Updates

---
+++ precache-manifest.e882b92ecc377ade40144adfcd034c47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a28d9d87ef73d5aab71e9cc37576f67",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "86c8716895814b2508e8",
+    "url": "/react_todo-app/static/css/main.3bd4d77a.chunk.css"
+  },
+  {
+    "revision": "2249d86949b54dc0e89e",
+    "url": "/react_todo-app/static/js/2.09aed800.chunk.js"
+  },
+  {
+    "revision": "86c8716895814b2508e8",
+    "url": "/react_todo-app/static/js/main.21310e11.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_todo-app/precache-manifest.36c0f0e73e4ba58e6c71741bdc35771c.js"
+  "/react_todo-app/precache-manifest.e882b92ecc377ade40144adfcd034c47.js"
 );
 
 self.addEventListener('message', (event) => {
